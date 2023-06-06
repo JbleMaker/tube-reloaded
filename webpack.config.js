c9945be7700b833e656e686532da283fcbd60144
JbleMaker <jbojsun@gmail.com>
2023-06-06 13:04:54 +0900

    webpack upload

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/js/main.js", //처리하고 싶은 파일, 변경하고 싶은 파일
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  mode: "development",
+  watch: true,
+  output: {
+    //작업이 끝난 후의 파일을 ./assets/js/ 디렉토리에 저장
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        //웹팩은 뒤에서부터 진행
+      },
+    ],
+  },
+};
